308df111899d859796ca5baab94fd16872299dfd
mzp <mzpppp@gmail.com>
2017-05-09 23:23:00 +0900

    lwt ping/pong example

--- src/index.ml
+++ src/index.ml
@@ -1,7 +1,30 @@
-let thread_1 =
-  Lwt.return "hello world"
+open Lwt
+
+let ping_cond : unit Lwt_condition.t =
+  Lwt_condition.create ()
+
+let pong_cond : unit Lwt_condition.t =
+  Lwt_condition.create ()
+
+let rec ping () =
+  Lwt_condition.wait ping_cond >>= begin fun () ->
+    print_endline "O\t->";
+    Lwt_condition.broadcast pong_cond ();
+    ping ()
+  end
+
+let rec pong () =
+  Lwt_condition.wait pong_cond >>= begin fun () ->
+    print_endline "\t<-\tO";
+    Lwt_condition.broadcast ping_cond ();
+    pong ()
+  end
 
 let _ =
-  Lwt.(
-    thread_1
-    >>= (fun s -> print_endline s; return s))
+  Lwt_main.run @@ join [
+    ping ();
+    pong ();
+
+    (* fire condition *)
+    wrap (fun () -> Lwt_condition.broadcast ping_cond ())
+  ]
